10e758e08979bd07a8b5b10d1fef56a51136c65c
YuzheIce <yuzhe.wang@foxmail.com>
2021-01-10 17:18:45 +0800

    modify env.workspace

--- pycor_dem2dem_standalone_addmessage.py
+++ pycor_dem2dem_standalone_addmessage.py
@@ -44,10 +44,13 @@ else:
     raise LicenseError
 
 # Set environment workspace in current directory
-env.workspace = r"E:\Mix\WYZ_AcademicWriting\paper_pycor\demo_data"
+path_script = os.path.dirname(os.path.abspath(__file__))
+env.workspace = os.path.join(path_script, 'demo_data', 'benchmark_data')
+
+#env.workspace = r"E:\Mix\WYZ_AcademicWriting\paper_pycor\arcPycor\demo_data\benchmark_data"
 
 # Folder for outputs
-dirOutputs= os.path.join(env.workspace, 'outputs')
+dirOutputs = os.path.join(env.workspace, 'outputs')
 if os.path.exists(dirOutputs):
     shutil.rmtree(dirOutputs)
 os.makedirs(dirOutputs)
